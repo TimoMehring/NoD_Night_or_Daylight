18a3794817284570d46b8dc2101e80e912d93b75
Timo Mehring <timo0mehringgmail.com>
2026-08-09 23:35:21 +0200

    Added Windows .exe icon [resources.rc, build_release.bat]

--- build_release.bat
+++ build_release.bat
@@ -1,10 +1,23 @@
 @echo off
 cd /d %~dp0
 
+
+if not exist build mkdir build
 if not exist release mkdir release
 if not exist release\NoD_v1.0.0 mkdir release\NoD_v1.0.0
 
-g++ -std=c++17 -O2 src/main.cpp src/graphics.cpp src/daytime.cpp src/audio.cpp -o release\NoD_v1.0.0\NoD.exe -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows
+
+windres resources.rc -O coff -o build\resources.o
+
+if %errorlevel% neq 0 (
+    echo.
+    echo Resource Build fehlgeschlagen.
+    pause
+    exit /b %errorlevel%
+)
+
+
+g++ -std=c++17 -O2 src/main.cpp src/graphics.cpp src/daytime.cpp src/audio.cpp build\resources.o -o release\NoD_v1.0.0\NoD.exe -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows
 
 if %errorlevel% neq 0 (
     echo.
